d36b79a9f6975f2446506827bf2b83a922f3f88f
matt3327 <68668319+matt3327@users.noreply.github.com>
2021-01-27 22:00:54 -0500

    homework1

---
+++ src/PartF.java
@@ -0,0 +1,31 @@
+
+public class PartF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=1; i<5; i ++)
+		{
+		    for (int k=0; k < (4 - i); k++)
+		    {
+		        System.out.print(" ");
+		    }
+		    for (int j=0; j<i; j++)
+		    {
+		        System.out.print("* ");
+		    }
+		    System.out.println("");
+		}
+		for (int i=1; i<5; i ++)
+		{
+		    for (int k=4; k > (4 - i); k--)
+		    {
+		        System.out.print(" ");
+		    }
+		    for(int l = 4; l > i; l--) {
+		    	System.out.print("* ");
+		    }
+		    System.out.println("");
+	}
+}
+}
+
